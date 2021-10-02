39ca135f39bc7fb9107ac6eee0b06cf1ceb5fe5b
popam <popa.mariusz@gmail.com>
2021-10-02 21:51:47 +0200

    kopia3

--- main.py
+++ main.py
@@ -21,3 +21,18 @@ class WeatherForecast:
     def get_temperature(self):
         avg_temp = self.data['forecast']['forecastday'][0]['day']['avgtemp_c']
         return avg_temp
+
+    def get_rain_info(self):
+        totalprecip_mm = float(self.data['forecast']['forecastday'][0]['day']['totalprecip_mm'])
+        return self.get_rain_chance(totalprecip_mm)
+
+    def get_rain_chance(self, totalprecip_mm):
+        if totalprecip_mm > 0.0:
+            return "Będzie padać"
+        elif totalprecip_mm == 0.0:
+            return "Nie będzie padać"
+        return "Nie wiem!"
+
+
+weather = WeatherForecast(api_key=sys.argv[1], date=sys.argv[2])
+print(weather.get_rain_info())
