f35bcfb64734fb763590a376ef1f7cc75a1213c1
popam <popa.mariusz@gmail.com>
2021-10-04 10:28:00 +0200

    dodawanie do bazy

--- main.py
+++ main.py
@@ -27,12 +27,14 @@ class WeatherForecast:
         totalprecip_mm = float(self.data['forecast']['forecastday'][0]['day']['totalprecip_mm'])
         return self.get_rain_chance(totalprecip_mm)
 
+
     def get_rain_chance(self, totalprecip_mm):
         if totalprecip_mm > 0.0:
-            return "Będzie padać"
+            return "Bedzie padac"
         elif totalprecip_mm == 0.0:
-            return "Nie będzie padać"
+            return "Nie bedzie padac"
         return "Nie wiem!"
+
 
 with open(f'weather_history.txt', 'r') as file:
     for line in file:
@@ -46,13 +48,22 @@ with open(f'weather_history.txt', 'r') as file:
         print(base_weather)
 
 weather = WeatherForecast(api_key=sys.argv[1], date=sys.argv[2])
-# print(weather.get_rain_info())
 
 date = sys.argv[2]
+element = []
+
 while True:
     if date == base_weather[0][0]:
-        print(date)
+        print(base_weather[0][1])
         break
     else:
         print(weather.get_rain_info())
+        value = weather.get_rain_info()
+        element.append(date)
+        element.append(value)
+        base_weather.append(element)
         break
+print(base_weather)
+with open(f'weather_history.txt', 'a') as file:
+    for line in base_weather:
+        file.write(f'{line[0]}' + ';' + f'{line[1]}' + '\n')
